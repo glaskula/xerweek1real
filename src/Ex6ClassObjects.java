877052a29cc0f2cf5b2c02e51eae8d673b51bd6d
philip <philip.rabia@hotmail.com>
2020-01-20 17:17:11 +0100

    Revert "2020-01-20 1500"
    This reverts commit 513b497d

--- src/Ex6ClassObjects.java
+++ src/Ex6ClassObjects.java
@@ -20,34 +20,10 @@ public class Ex6ClassObjects {
 
     void program() {
         // -------- Input ------------
-        out.print("What's the name of the hero 1? ");
-        String hero1_name = sc.nextLine();
-        out.print("How strong is "+hero1_name+"?");
-        int hero1_strength = sc.nextInt();
-        sc.nextLine();
 
-        out.print("What's the name of the hero 2? ");
-        String hero2_name = sc.nextLine();
-        out.print("How strong is "+hero2_name+"?");
-        int hero2_strength = sc.nextInt();
 
-        Hero hero1 = new Hero();
-        hero1.name = hero1_name;
-        hero1.strength = hero1_strength;
-
-        Hero hero2 = new Hero();
-        hero2.name = hero2_name;
-        hero2.strength = hero2_strength;
         // ------- Output --------------
-        if(hero1.strength > hero2.strength) {
-            out.print("hero " + hero1.name + "is stronger");
-        }
-        else if(hero2.strength > hero1.strength){
-            out.print("hero " + hero2.name + " is stronger");
-        }
-        else {
-            out.print("They are equally strong");
-        }
+
     }
 
     // ------ The class to use  -----------
